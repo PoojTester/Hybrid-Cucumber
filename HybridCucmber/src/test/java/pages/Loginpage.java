0895cac4d8fb48f3c5262a26d88772d3d2919c8d
HP <HP@10.10.80.3>
2022-02-02 16:22:16 +0530

    Hybrid First Commit

--- HybridCucmber/src/test/java/pages/Loginpage.java
+++ HybridCucmber/src/test/java/pages/Loginpage.java
@@ -1,5 +1,33 @@
 package pages;
 
-public class Loginpage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+public class Loginpage {
+	WebDriver driver;
+	@FindBy(id="txtUsername")
+	WebElement username;
+	@FindBy(id="txtPassword")
+	WebElement password;
+	@FindBy(id="btnLogin")
+	WebElement login;
+	public Loginpage(WebDriver driver)
+	{
+		this.driver=driver;
+		PageFactory.initElements(driver ,this);
+	}
+	public void login(String uname,String psword)
+	{
+		username.sendKeys(uname);
+		password.sendKeys(psword);
+	
+		
+	}
+	
+	public void submit() {
+		login.click();
+		
+	}
 }
--- HybridCucmber/src/test/java/testcases/Logintestcases.java
+++ HybridCucmber/src/test/java/testcases/Logintestcases.java
@@ -1,5 +1,35 @@
 package testcases;
 
-public class Logintestcases {
+import java.io.IOException;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import baseclass.Libraryclass;
+import pages.Loginpage;
+import seleniumreuseablefunction.Seleniumutility;
+
+public class Logintestcases extends Libraryclass {
+	Seleniumutility selenium;
+	Loginpage lp;
+	@BeforeMethod
+	public void openapp()
+	{
+		launchapp("chrome","https://opensource-demo.orangehrmlive.com/");
+	}
+	@Test
+	public void login()
+	{
+		 lp=new Loginpage(driver);
+		lp.login("Admin","admin123");
+	}
+	
+	@AfterMethod
+	public void teardown() throws IOException
+	{
+	 selenium=new Seleniumutility(driver);
+		selenium.screenshot("C:\\Users\\Dell\\eclipse-workspace\\Shobiyaselenium\\src\\test\\resources\\Screenshots\\Login.png");
+	}
 }
+
